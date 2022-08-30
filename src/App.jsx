0ff0a1e955573371a3bcfd72b06ab4fc1dfff9ba
KirikWorld <kuleshovk455@gmail.com>
2022-08-30 10:24:57 +0300

    deleted files from other project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Counter from "./components/counterComponent";
 
 export default function App() {
     return (
         <div className="App">
-            <Counter />
+            
         </div>
     );
 }
