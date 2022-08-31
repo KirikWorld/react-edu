1b39adcaaa184a9dfb1adcc7265b6410c6aaa2c6
KirikWorld <kuleshovk455@gmail.com>
2022-09-01 00:33:50 +0300

    project finished and added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React from "react";
 export default function App() {
     return (
         <div className="App">
-            
+            <div className="container">
+                <div className="card">
+                    <div className="envelope"></div>
+                    <h1>
+                        Ты для меня как <em>CSS</em> для <em>HTML</em>
+                    </h1>
+                    <div className="heart"></div>
+                </div>
+            </div>
         </div>
     );
 }
